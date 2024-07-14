cf6a68d8e1fb42ef64bb97adf635ad9b7568e812
KOSASIH <kosasihg88@gmail.com>
2024-07-14 14:07:33 +0700

    Update model.py

--- src/backend/models/model.py
+++ src/backend/models/model.py
@@ -27,3 +27,29 @@ class DisasterPredictionModel:
 
     def predict_proba(self, input_data):
         return self.keras_model.predict(input_data)
+
+class PreventionMeasuresModel:
+    def __init__(self):
+        self.model = RandomForestClassifier()
+
+    def train(self, data):
+        X = data.drop(["target"], axis=1)
+        y = data["target"]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.model.fit(X_train, y_train)
+
+    def predict(self, input_data):
+        return self.model.predict(input_data)
+
+class MitigationMeasuresModel:
+    def __init__(self):
+        self.model = RandomForestClassifier()
+
+    def train(self, data):
+        X = data.drop(["target"], axis=1)
+        y = data["target"]
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        self.model.fit(X_train, y_train)
+
+    def predict(self, input_data):
+        return self.model.predict(input_data)
