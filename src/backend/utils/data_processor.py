4118e0baece6134851f1ccb3f033f555607c3cba
KOSASIH <kosasihg88@gmail.com>
2024-07-14 14:15:15 +0700

    Create data_processor.py

---
+++ src/backend/utils/data_processor.py
@@ -0,0 +1,8 @@
+# src/backend/utils/data_processor.py
+import pandas as pd
+
+class DataProcessor:
+    def process_realtime_data(self):
+        # simulate realtime data
+        data = {"labels": ["label1", "label2"], "values": [10, 20]}
+        return data
